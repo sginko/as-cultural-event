c263a12e74f4cf6b7a6f11e1e7f59b9e9b574ac7
Alena Iadkouskaya <koptewka@mail.ru>
2024-05-24 19:47:22 +0200

    add validation to class AccountEntity

--- src/main/java/com/example/cultural_event/account/AccountEntity.java
+++ src/main/java/com/example/cultural_event/account/AccountEntity.java
@@ -15,8 +15,22 @@ public class AccountEntity {
     private String email;
 
     public AccountEntity(String name, String city, String email) {
+        validateName(name);
+        validateEmail(email);
         this.name = name;
         this.city = city;
         this.email = email;
+    }
+
+    private void validateEmail(String email) {
+        if (!email.contains("@") || email.trim().contains(" ")) {
+            throw new AccountException("Check your email!");
+        }
+    }
+
+    private void validateName(String name) {
+        if (name.trim().length() == 0) {
+            throw new AccountException("Name can not be empty!");
+        }
     }
 }
---
+++ src/main/java/com/example/cultural_event/account/AccountException.java
@@ -0,0 +1,7 @@
+package com.example.cultural_event.account;
+
+public class AccountException extends RuntimeException{
+    public AccountException(String message) {
+        super(message);
+    }
+}
