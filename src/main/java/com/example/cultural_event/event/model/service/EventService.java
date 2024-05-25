c020d62cff56c2e933cdf53261689a04cadb02ba
sginko <142085017+sginko@users.noreply.github.com>
2024-05-25 12:36:58 +0200

    add get controller (#4)
    method findAllEvents

--- src/main/java/com/example/cultural_event/event/model/service/EventService.java
+++ src/main/java/com/example/cultural_event/event/model/service/EventService.java
@@ -1,7 +1,11 @@
 package com.example.cultural_event.event.model.service;
 
 import com.example.cultural_event.event.model.dto.EventRequestDto;
+import com.example.cultural_event.event.model.dto.EventResponseDto;
+
+import java.util.List;
 
 public interface EventService {
     void addEvent(EventRequestDto eventRequestDto);
+    List<EventResponseDto> findAllEvents();
 }
--- src/main/java/com/example/cultural_event/event/model/service/EventServiceImpl.java
+++ src/main/java/com/example/cultural_event/event/model/service/EventServiceImpl.java
@@ -1,14 +1,17 @@
 package com.example.cultural_event.event.model.service;
 
 import com.example.cultural_event.event.model.dto.EventRequestDto;
+import com.example.cultural_event.event.model.dto.EventResponseDto;
 import com.example.cultural_event.event.model.mapper.EventMapper;
 import com.example.cultural_event.event.model.repository.EventRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class EventServiceImpl implements EventService{
-   private final EventRepository eventRepository;
-   private final EventMapper eventMapper;
+public class EventServiceImpl implements EventService {
+    private final EventRepository eventRepository;
+    private final EventMapper eventMapper;
 
     public EventServiceImpl(EventRepository eventRepository, EventMapper eventMapper) {
         this.eventRepository = eventRepository;
@@ -18,5 +21,12 @@ public class EventServiceImpl implements EventService{
     @Override
     public void addEvent(EventRequestDto eventRequestDto) {
         eventRepository.save(eventMapper.toEntity(eventRequestDto));
+    }
+
+    @Override
+    public List<EventResponseDto> findAllEvents() {
+        return eventRepository.findAll().stream()
+                .map(eventEntity -> eventMapper.fromEntity(eventEntity))
+                .toList();
     }
 }
