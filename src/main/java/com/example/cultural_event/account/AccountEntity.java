48b4eaf729857dd6536d9a73835415f7aa06e46b
Alena Iadkouskaya <koptewka@mail.ru>
2024-05-26 14:18:40 +0200

    fixed bags, review code

--- src/main/java/com/example/cultural_event/account/AccountEntity.java
+++ src/main/java/com/example/cultural_event/account/AccountEntity.java
@@ -1,26 +1,19 @@
 package com.example.cultural_event.account;
 
-import com.example.cultural_event.event.model.enity.EventEntity;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.util.HashSet;
-import java.util.Set;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 @Getter
 @Table(name = "accounts")
 public class AccountEntity {
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    private Set<String> listNotification;
     private String name;
     private String city;
     private String email;
@@ -28,7 +21,6 @@ public class AccountEntity {
     public AccountEntity(String name, String city, String email) {
         validateName(name);
         validateEmail(email);
-        this.listNotification = new HashSet<>();
         this.name = name;
         this.city = city;
         this.email = email;
@@ -47,9 +39,7 @@ public class AccountEntity {
     }
 
     public void receiveNotification(String eventName) {
-        //System.out.println("Notification for account " + name + ": Event " + eventName + " has been created. In work...");
-        listNotification.add("Notification about event " + eventName + " in your city " + city);
+        System.out.println("Notification for account " + name + ": Event " + eventName + " has been created. In work...");
     }
-
 
 }
--- src/main/java/com/example/cultural_event/notification/service/NotificationServiceImpl.java
+++ src/main/java/com/example/cultural_event/notification/service/NotificationServiceImpl.java
@@ -3,6 +3,8 @@ package com.example.cultural_event.notification.service;
 import com.example.cultural_event.account.AccountEntity;
 import com.example.cultural_event.account.AccountReader;
 import com.example.cultural_event.event.model.enity.EventEntity;
+import com.example.cultural_event.notification.NotificationEntity;
+import com.example.cultural_event.notification.NotificationRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
@@ -12,17 +14,22 @@ import java.util.List;
 public class NotificationServiceImpl implements NotificationService, NotifyListener {
 
     private final AccountReader accountReader;
+    private final NotificationRepository notificationRepository;
 
-    public NotificationServiceImpl(AccountReader accountReader) {
+    public NotificationServiceImpl(AccountReader accountReader, NotificationRepository notificationRepository) {
         this.accountReader = accountReader;
+        this.notificationRepository = notificationRepository;
     }
 
     public void createNotifications(EventEntity event, List<AccountEntity> accounts) {
         sendNotifications(event, accounts);
     }
 
+    @Transactional
     public void sendNotifications(EventEntity event, List<AccountEntity> accounts) {
         for (AccountEntity account : accounts) {
+            NotificationEntity notificationEntity = new NotificationEntity(event, account, "Notification about " + event.getEventName() + " for your city " + event.getCity());
+            notificationRepository.save(notificationEntity);
             account.receiveNotification(event.getEventName());
         }
     }
