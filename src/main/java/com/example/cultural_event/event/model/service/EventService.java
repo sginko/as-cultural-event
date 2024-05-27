af072f0c1a781e9cb51907ea2df64f887ca9d1fc
Alena Iadkouskaya <koptewka@mail.ru>
2024-05-27 17:02:53 +0200

    fixed

--- src/main/java/com/example/cultural_event/event/model/service/EventService.java
+++ src/main/java/com/example/cultural_event/event/model/service/EventService.java
@@ -1,6 +1,7 @@
 package com.example.cultural_event.event.model.service;
 
 import com.example.cultural_event.event.model.dto.EventRequestDto;
+import com.example.cultural_event.event.model.dto.EventResponseDto;
 
 import java.util.List;
 
