5f13efc4f5f05fc0a75ada5e5769e43b8c9ab822
Alena Iadkouskaya <koptewka@mail.ru>
2024-05-27 14:51:56 +0200

    fixed

--- src/test/java/com/example/cultural_event/account/services/AccountServiceImplTest.java
+++ src/test/java/com/example/cultural_event/account/services/AccountServiceImplTest.java
@@ -3,12 +3,18 @@ package com.example.cultural_event.account.services;
 import com.example.cultural_event.account.AccountEntity;
 import com.example.cultural_event.account.AccountRepository;
 import com.example.cultural_event.account.AccountRequestDto;
+import com.example.cultural_event.event.model.dto.EventRequestDto;
+import com.example.cultural_event.event.model.enity.EventEntity;
+import com.example.cultural_event.event.model.service.EventService;
+import com.example.cultural_event.notification.NotificationEntity;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -19,6 +25,8 @@ class AccountServiceImplTest {
     private final String CITY = "South Luthermouth";
     @Autowired
     private AccountService accountService;
+    @Autowired
+    private EventService eventService;
     @Autowired
     private AccountRepository accountRepository;
 
@@ -38,8 +46,23 @@ class AccountServiceImplTest {
         //then
         assertThat(all.size()).isEqualTo(1);
     }
+    @Test
+    public void should_find_all_notifications_for_account_by_id() {
+        //given
+        AccountRequestDto accountRequestDto = new AccountRequestDto(CORRECT_NAME, CITY, CORRECT_EMAIL);
 
+        accountService.addNewAccount(accountRequestDto);
+        List<AccountEntity> all = accountRepository.findAll();
+        UUID technicalId = all.get(0).getTechnicalId();
+        EventRequestDto event = new EventRequestDto("event", CITY, LocalDateTime.now());
+        eventService.addEvent(event);
+        //when
+        List<NotificationEntity> allNotifications = accountService.findAllNotifications(technicalId);
+        //then
+        assertThat(allNotifications.size()).isEqualTo(1);
+    }
     private AccountRequestDto prepareAccountRequestDto(String name, String city, String email) {
         return new AccountRequestDto(name, city, email);
     }
+
 }
