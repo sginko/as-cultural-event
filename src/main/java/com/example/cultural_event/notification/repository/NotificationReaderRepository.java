a9d0112707beb2d870fcf7925d4567d417b516cb
Alena Iadkouskaya <117815613+LenKopt@users.noreply.github.com>
2024-06-03 20:17:29 +0200

    version_2_basic, is ready (#17)
    Co-authored-by: Alena Iadkouskaya <koptewka@mail.ru>

--- src/main/java/com/example/cultural_event/notification/repository/NotificationReaderRepository.java
+++ src/main/java/com/example/cultural_event/notification/repository/NotificationReaderRepository.java
@@ -12,10 +12,12 @@ import java.util.UUID;
 @Repository
 public interface NotificationReaderRepository extends JpaRepository<NotificationEntity, Long> {
     @Transactional
-    @Query("SELECT n FROM NotificationEntity n WHERE n.account.technicalId = :technicalId")
+    @Query("SELECT n FROM NotificationEntity n WHERE n.city = (SELECT a.city FROM AccountEntity a where a.technicalId=:technicalId)")
+    //"SELECT n FROM NotificationEntity n WHERE n.city = (SELECT a.city FROM AccountEntity a WHERE a.city = :city)"
     List<NotificationEntity> getAllNotificationsForAccount(UUID technicalId);
-
+//
     @Transactional
-    @Query("SELECT n FROM NotificationEntity n WHERE n.account.technicalId = :technicalId AND EXISTS (SELECT s FROM SubscriptionEntity s WHERE s.account=n.account AND s.events=n.events)")
-    List<NotificationEntity> getAllNotificationsForAccountWithSubscription(UUID technicalId);
+    //@Query("SELECT n FROM NotificationEntity n WHERE n.account.technicalId = :technicalId AND EXISTS (SELECT s FROM SubscriptionEntity s WHERE s.account=n.account AND s.events=n.events)")
+    @Query("SELECT 1")
+   List<NotificationEntity> getAllNotificationsForAccountWithSubscription(UUID technicalId);
 }
