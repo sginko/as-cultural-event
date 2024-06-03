7c69c3b28cbb07a9b901145be640077876e54f69
sginko <s.ginkota@gmail.com>
2024-06-03 11:21:54 +0200

    some changes

--- src/main/java/com/example/cultural_event/account/dto/AccountRequestDto.java
+++ src/main/java/com/example/cultural_event/account/dto/AccountRequestDto.java
@@ -5,10 +5,13 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.UUID;
+
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class AccountRequestDto {
+    private UUID technicalId;
     private String name;
     private String city;
     private String email;
